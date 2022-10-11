0423b68d7945e895c18974dfd8d0d3d42ac4c9f8
sezgin <sezgin.akgull@gmail.com>
2022-10-11 22:40:36 +0300

    project

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import "./Main.scss";
+import { data } from "../../helper/data";
+import Card from "./Card";
+
+const Main = () => {
+  return (
+    <div className="card-container">
+      {data.map((item, index) => (
+        <Card {...item} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default Main;
